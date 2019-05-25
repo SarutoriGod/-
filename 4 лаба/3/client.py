4ea18918426c629313b5e4d004c73d19112c0af8
SarutoriGod <48445084+SarutoriGod@users.noreply.github.com>
2019-05-25 14:26:43 +0300

    Add files via upload

--- 4 лаба/3/client.py
+++ 4 лаба/3/client.py
@@ -3,11 +3,10 @@ import socket
 
 sock = socket.socket()
 sock.connect(('127.0.0.1', 5000))
-matrix = ''
-for i in range(1,4):
-	print("Введите ", i, " строчку: ")
-	matrix += input() + ';'
+matrix = []
+for i in range(0,3):
+	matrix.append(input(f'Введите {i+1}-ю строку: '))
 
 
-sock.send(matrix.encode())
+sock.send(str(matrix).encode())
 print((sock.recv(128).decode()))
--- 4 лаба/3/server.py
+++ 4 лаба/3/server.py
@@ -1,10 +1,10 @@
 import socket
 
 
-def op(new_matrix):
-	return (new_matrix[0][0] * (new_matrix[1][1] * new_matrix[2][2] - new_matrix[2][1] * new_matrix[1][2]) -
-		   new_matrix[0][1] * (new_matrix[1][0] * new_matrix[2][2] - new_matrix[2][0] * new_matrix[1][2]) +
-		   new_matrix[0][2] * (new_matrix[1][0] * new_matrix[2][1] - new_matrix[2][0] * new_matrix[1][1]))
+def op(matrix):
+	return (matrix[0] * (matrix[4] * matrix[8] - matrix[7] * matrix[5]) -
+			matrix[1] * (matrix[3] * matrix[8] - matrix[6] * matrix[5]) +
+			matrix[2] * (matrix[3] * matrix[7] - matrix[6] * matrix[4]))
 
 
 new_matrix = []
@@ -12,9 +12,5 @@ sock = socket.socket()
 sock.bind(('', 5000))
 sock.listen()
 connectrion, addr = sock.accept()
-matrix = (connectrion.recv(128)).decode().strip()
-matrix = [int(x) for x in matrix if x.isdigit()]
-for i in range(0,3):
-	new_matrix.append(matrix[i*3:i*3+3:1])
-print(new_matrix)
-connectrion.send(str((op(new_matrix))).encode())
+matrix = [int(x) for x in connectrion.recv(128).decode() if x.isdigit()]
+connectrion.send(str((op(matrix))).encode())
