70d14311dc3fe5d1ef252c2ebe66ee0479f98bef
SarutoriGod <48445084+SarutoriGod@users.noreply.github.com>
2019-05-25 14:25:34 +0300

    Delete client.py

--- 4 лаба/2/client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-
-sock = socket.socket()
-sock.connect(('127.0.0.1', 5000))
-message = input("Введите значения переменных через пробел: ")
-sock.send(message.encode())
-print(sock.recv(128).decode())
-sock.close()
